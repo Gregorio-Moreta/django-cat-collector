9a9220564ff75d7d5b17a1f34897ecef5ec62f21
Gregorio-Moreta <78994816+Gregorio-Moreta@users.noreply.github.com>
2024-02-04 18:18:51 -0500

    added Toy model, admin, serializer, urls, and views. Also ran makemigrations and migrate commands to update the psql database with the new model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 # import your models here
-from .models import Cat, Feeding
+from .models import Cat, Feeding, Toy
 
 # Register your models here
 admin.site.register(Cat)
 
 # register the new Feeding Model 
 admin.site.register(Feeding)
+
+# add the Toy model
+admin.site.register(Toy)
