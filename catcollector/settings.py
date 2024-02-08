29bf1de13cc388ec9ccc8f981abcc085d7945664
Gregorio-Moreta <78994816+Gregorio-Moreta@users.noreply.github.com>
2024-02-07 23:17:37 -0500

    installed deployment packages, created Procfile, updated settings.py with all necessary deployment checklist items.

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -14,6 +14,24 @@ from pathlib import Path
 # Configuration for simple JWT
 from datetime import timedelta
 
+# Deployment imports
+import dj_database_url
+import django_heroku
+import environ  
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+
+# Load up the env variables for use in this file
+SECRET_KEY=env('SECRET_KEY')
+PGDATABASE=env('PGDATABASE')
+PGHOST=env('PGHOST')
+PGPASSWORD=env('PGPASSWORD')
+PGPORT=env('PGPORT')
+PGUSER=env('PGUSER')
+DATABASE_URL=env('DATABASE_URL')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +45,9 @@ SECRET_KEY = 'django-insecure-5xboz%bsx#farthviv^0*g9%vx0srac-epwxqh3sh!mm6f4403
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1'] # allows requests from localhost - will need to update again for deployment
+# catcollector/settings.py
+
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com'] # allows requests from localhost - will need to update again for deployment
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
@@ -51,6 +71,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -109,16 +130,22 @@ WSGI_APPLICATION = 'catcollector.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# Deployment database config to work with heroku
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'catcollector',
-        # 'HOST': 'localhost',  <-- (optional) some computers might need this line
-        # 'USER': 'cat_admin', <-- (optional) postgres user name, if you have to sign into an account to open psql, you will want to add that user name here.
-        # 'PASSWORD': 'password', <-- (optional) postgres user password, if you have to sign into an account to open psql, you will want to add that user password here.
-        # 'PORT': 3000 <-- if you desire to use a port other than 8000, you can change that here to any valid port id, some number between 1 and 65535 that isn't in use by some other process on your machine. The reason for this port number range is because of how TCP/IP works, a TCP/IP protocol network(the most widely used protocol used on the web) allocated 16 bits for port numbers. This means that number must be greater than 0 and less than 2^15 -1. 
-    }
+    'default': 
+        dj_database_url.config('DATABASE_URL')
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'catcollector',
+#         # 'HOST': 'localhost',  <-- (optional) some computers might need this line
+#         # 'USER': 'cat_admin', <-- (optional) postgres user name, if you have to sign into an account to open psql, you will want to add that user name here.
+#         # 'PASSWORD': 'password', <-- (optional) postgres user password, if you have to sign into an account to open psql, you will want to add that user password here.
+#         # 'PORT': 3000 <-- if you desire to use a port other than 8000, you can change that here to any valid port id, some number between 1 and 65535 that isn't in use by some other process on your machine. The reason for this port number range is because of how TCP/IP works, a TCP/IP protocol network(the most widely used protocol used on the web) allocated 16 bits for port numbers. This means that number must be greater than 0 and less than 2^15 -1. 
+#     }
+# }
 
 
 # Password validation
@@ -161,3 +188,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Add this to the very bottom of your settings.py file
+# If you don't your app will not deploy properly
+django_heroku.settings(locals())
